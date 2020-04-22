926a8b1f7fe80f658fa22e6ec8ce015432344f3b
Neea Rusch <neea.rusch@mobilefirst.me>
2020-04-22 12:56:28 -0400

    refactor service class, convert to kotlin

---
+++ base/src/main/java/mf/asciitext/MyInputMethodService.kt
@@ -0,0 +1,262 @@
+@file:Suppress("DEPRECATION")
+
+package mf.asciitext
+
+import android.content.Context
+import android.content.Intent
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener
+import android.text.TextUtils
+import android.view.KeyEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import mf.asciitext.fonts.AppFont
+import mf.asciitext.fonts.AvailableFonts.getFonts
+
+/**
+ * This class sets up and handles virtual keyboard events
+ */
+class MyInputMethodService : InputMethodService(), OnKeyboardActionListener {
+    // Primary vs. secondary keyboards
+    private val ALPHA_KEYBOARD_KEYCODE = -10
+    private val SECONDARY_KBD_KEYCODE = -11
+
+    // All available font styles
+    private val fonts = getFonts()
+
+    // Font with "no style"
+    private val REGULAR_FONT_INDEX = -1
+
+    // Keyboard variations
+    private val ALPHA_KBD = 0
+    private val NUMBER_KBD = 1
+    private val MATH_KBD = 2
+
+    // UI Elements
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+    private var fontStyleToggle: AppCompatImageView? = null
+    private var fontPicker: RecyclerView? = null
+    private var adapter: FontPickerAdapter? = null
+
+    // keyboard default state
+    private var keyboardChoice = ALPHA_KBD
+    private var fontIndex = REGULAR_FONT_INDEX
+    private var lastSelectedStyleIndex = REGULAR_FONT_INDEX
+
+    override fun onCreateInputView(): View {
+
+        val layout = layoutInflater.inflate(R.layout.keyboard_view, null)
+        val ctx = layout.context
+
+        /* initialize keyboard */
+
+        keyboardView = layout.findViewById(R.id.keyboard_view)
+        keyboard = Keyboard(this, R.xml.keyboard)
+        keyboardView?.keyboard = keyboard
+        keyboardView?.setOnKeyboardActionListener(this)
+
+        /* setup font picker recyclerView */
+
+        fontPicker = layout.findViewById(R.id.fontPicker)
+        adapter = FontPickerAdapter(fonts, onFontSelection())
+        val layoutManager = GridLayoutManager(
+            ctx, 1, LinearLayoutManager.HORIZONTAL, false
+        )
+        fontPicker?.layoutManager = layoutManager
+        fontPicker?.adapter = adapter
+        adapter!!.setSelectedFont(fontIndex)
+
+        /* setup UI icon buttons */
+
+        fontStyleToggle = layout.findViewById(R.id.font_button)
+        fontStyleToggle?.setOnClickListener(onFontButtonClick())
+        setFontStyleIcon(fontIndex == REGULAR_FONT_INDEX)
+        val settingsButton: AppCompatImageView = layout.findViewById(R.id.settings_button)
+        settingsButton.setOnClickListener(onSettingsClick(ctx))
+        return layout
+    }
+
+    /**
+     * Handle keyboard key presses
+     */
+    override fun onKey(primaryCode: Int, keyCodes: IntArray) {
+        if (currentInputConnection != null) {
+            when (primaryCode) {
+                SECONDARY_KBD_KEYCODE -> toggleExtendedKeyboardView()
+                ALPHA_KEYBOARD_KEYCODE -> enableAlphaKeyboard()
+                Keyboard.KEYCODE_DELETE -> handleDeleteKeyPress()
+                Keyboard.KEYCODE_SHIFT -> handleShiftKeyPress()
+                Keyboard.KEYCODE_DONE -> handleDoneKeyPress()
+                else -> encodeCharacter(primaryCode)
+            }
+        }
+    }
+
+    override fun onPress(i: Int) {}
+    override fun onRelease(i: Int) {}
+    override fun onText(charSequence: CharSequence) {}
+    override fun swipeLeft() {}
+    override fun swipeRight() {}
+    override fun swipeDown() {}
+    override fun swipeUp() {}
+
+    /**
+     * Switch between numeric and symbolic keyboard
+     */
+    private fun toggleExtendedKeyboardView() {
+        if (keyboardChoice == NUMBER_KBD) {
+            keyboard = Keyboard(this, R.xml.keyboard_math)
+            keyboardChoice = MATH_KBD
+        } else {
+            keyboard = Keyboard(this, R.xml.keyboard_extended)
+            keyboardChoice = NUMBER_KBD
+        }
+        keyboardView!!.keyboard = keyboard
+        keyboardView!!.invalidateAllKeys()
+    }
+
+    /**
+     * Change view to alphabetic keyboard
+     */
+    private fun enableAlphaKeyboard() {
+        keyboard = Keyboard(this, R.xml.keyboard)
+        keyboardChoice = ALPHA_KBD
+        setShiftKeyIcon()
+        keyboardView!!.keyboard = keyboard
+        keyboardView!!.invalidateAllKeys()
+    }
+
+    /**
+     * When user clicks shift key
+     */
+    private fun handleShiftKeyPress() {
+        keyboard!!.isShifted = !keyboard!!.isShifted
+        setShiftKeyIcon()
+        keyboardView!!.invalidateAllKeys()
+    }
+
+    /**
+     * Handle keyboard delete key press
+     */
+    private fun handleDeleteKeyPress() {
+        val inputConnection = currentInputConnection
+        val selectedText = inputConnection.getSelectedText(0)
+        if (TextUtils.isEmpty(selectedText)) {
+            inputConnection.deleteSurroundingText(1, 0)
+        } else {
+            inputConnection.commitText("", 1)
+        }
+    }
+
+    /**
+     * Clicking on done key inserts a line break
+     */
+    private fun handleDoneKeyPress() {
+        val inputConnection = currentInputConnection
+        inputConnection.sendKeyEvent(
+            KeyEvent(
+                KeyEvent.ACTION_DOWN,
+                KeyEvent.KEYCODE_ENTER
+            )
+        )
+    }
+
+    /**
+     * Default keyboard key press handler
+     *
+     * @param primaryCode pressed key code
+     */
+    private fun encodeCharacter(primaryCode: Int) {
+        val inputConnection = currentInputConnection
+        var code = primaryCode.toChar()
+        if (Character.isLetter(code) && keyboard!!.isShifted) {
+            code = Character.toUpperCase(code)
+        }
+        var text = code.toString()
+        if (fontIndex >= 0 && fontIndex < fonts.size) {
+            val style = fonts[fontIndex]
+            text = style.encode(text).toString()
+        }
+        inputConnection.commitText(text, 1)
+    }
+
+    /**
+     * Handle font change
+     * Keep track of previous selection before current to enable
+     * style on-off toggle;
+     */
+    private fun onFontSelection(): FontPickerAdapter.OnItemClickListener {
+        return object : FontPickerAdapter.OnItemClickListener {
+            override fun onItemClick(item: AppFont?, index: Int) {
+                val previous = lastSelectedStyleIndex
+                fontIndex = index
+                lastSelectedStyleIndex = index
+                setFontStyleIcon(false)
+                adapter!!.setSelectedFont(fontIndex)
+                adapter!!.notifyItemChanged(fontIndex)
+                adapter!!.notifyItemChanged(previous)
+            }
+        }
+    }
+
+    /**
+     * Settings button click launches settings activity
+     */
+    private fun onSettingsClick(context: Context): View.OnClickListener {
+        return View.OnClickListener {
+            val intent = Intent(context, SettingsActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Clicking font icon toggles styled font on and off.
+     * OFF: type in regular letter case
+     * ON: restore previous styled font (if selection exists)
+     */
+    private fun onFontButtonClick(): View.OnClickListener {
+        return View.OnClickListener {
+            val disable = fontIndex != REGULAR_FONT_INDEX
+            fontIndex = if (disable) REGULAR_FONT_INDEX else lastSelectedStyleIndex
+            setFontStyleIcon(disable)
+            adapter!!.setSelectedFont(fontIndex)
+        }
+    }
+
+    /**
+     * Update font icon based on if custom styles are enabled/disabled
+     *
+     * @param disable - custom styles are disabled
+     */
+    private fun setFontStyleIcon(disable: Boolean) {
+        fontStyleToggle!!.setImageResource(
+            if (disable) R.drawable.ic_font_off
+            else R.drawable.ic_format_font
+        )
+        fontPicker!!.alpha = if (disable) 0.5f else 1.0f
+    }
+
+    /**
+     * Update keyboard shift key icon to match caps lock state
+     */
+    private fun setShiftKeyIcon() {
+        val keys = keyboard!!.keys
+        for (n in 0 until keys.size - 1) {
+            val currentKey = keys[n]
+            if (currentKey.codes[0] == Keyboard.KEYCODE_SHIFT) {
+                currentKey.icon = resources.getDrawable(
+                    if (keyboard!!.isShifted) R.drawable.ic_keyboard_caps_filled
+                    else R.drawable.ic_arrow_up_bold_outline
+                )
+                break
+            }
+        }
+    }
+}
