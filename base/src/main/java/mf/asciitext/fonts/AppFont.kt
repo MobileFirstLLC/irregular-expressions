1f2f09f0720dabc12db42c7f48b94773f6ed5ce6
Neea Rusch <neea.rusch@mobilefirst.me>
2020-06-05 12:59:12 -0400

    #11 add sequenceAware property to fonts

--- base/src/main/java/mf/asciitext/fonts/AppFont.kt
+++ base/src/main/java/mf/asciitext/fonts/AppFont.kt
@@ -3,13 +3,27 @@ package mf.asciitext.fonts
 abstract class AppFont(
     protected var id: String,
     protected var name: String,
-    protected var reverse: Boolean = false
+
+    /**
+     * This property should be set true for font styles
+     * that typed in RIGHT -> LEFT direction
+     */
+    protected var reverse: Boolean = false,
+
+    /**
+     * This property should be set true for fonts where
+     * encoding depends on a sequence of preceding characters
+     */
+    protected var sequenceAware: Boolean = false
 ) {
     val styledName: String
         get() = encode(name)!!
 
     val isReversed : Boolean
         get() = reverse
+
+    val isSequenceAware : Boolean
+        get() = sequenceAware
 
     fun isEmpty(text: String?): Boolean {
         return text == null || text.isEmpty()
