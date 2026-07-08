5eceea3ca319c0dc6f489c20076abb447c7bf86f
Neea <neearusch@gmail.com>
2026-07-09 00:52:55 +0200

    should add some tests, ow-wee

--- base/src/test/java/mf/irregex/ExampleUnitTest.kt
+++ base/src/test/java/mf/irregex/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package mf.irregex
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+//import org.junit.Assert.assertEquals
+//import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +9,8 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
 }
